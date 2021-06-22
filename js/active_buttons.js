c99ad7a990360febfec1493aebc3d51c8031b43d
Rodion Romanenko <rod.romanenko@gmail.com>
2021-06-22 07:20:10 +0400

    active button added

---
+++ js/active_buttons.js
@@ -0,0 +1,18 @@
+// const filterBtnContainer = document.querySelector('.filter-button-container');
+
+// filterBtnContainer.addEventListener('click', onfilterBtnContainerClick);
+
+// function onfilterBtnContainerClick(evt) {
+//     if (evt.target.nodeName !== 'BUTTON') {
+//         return;
+//     }
+
+//     const currentActiveBtn = document.querySelector('.current');
+
+//     if (currentActiveBtn) {
+//         currentActiveBtn.classList.remove('.current');
+//     }
+
+//     const nextActiveBtn = evt.target;
+//     nextActiveBtn.classList.add('.current');
+// }
